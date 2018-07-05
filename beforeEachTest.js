7089a6d7c999554bc5310c8ac224c71c3962186c
YazhiniNSR <40856200+YazhiniNSR@users.noreply.github.com>
2018-07-05 11:25:03 +0100

    Create beforeEachTest.js

---
+++ beforeEachTest.js
@@ -0,0 +1,17 @@
+"use strict";
+
+describe ("A new block", function() 
+{
+  beforeEach (function()
+  {
+      throw new Error('Error in beforeEach');
+  });
+  it (" First test", function()
+  {
+      console.log ("Executed first it block");
+  }):
+  it (" second test" , function ()
+  {
+      console.log ("Executed second it block");
+   });
+});
